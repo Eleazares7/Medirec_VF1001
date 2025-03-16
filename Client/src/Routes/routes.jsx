3333008e94955f96d6fc209143fe58e3893c3a75
Eleazar <eleazarprogrammeres7@gmail.com>
2025-03-15 18:51:27 -0600

    Fix last commit- Feature add patient, add page Login

--- Client/src/Routes/routes.jsx
+++ Client/src/Routes/routes.jsx
@@ -2,12 +2,14 @@ import Home from "../Pages/Home";
 import ServicesSection from "../Pages/Services";
 import MedicalReportsSection from "../Pages/MedicalReportSession";
 import RegisterPatient from "../Pages/Patients/RegisterPatient";
+import Login from "../Pages/Login";
 
 export const routes = [
     { path: '/', element: <Home />, index: true },
     { path: '/services', element: <ServicesSection /> },
     { path: '/medicalAppointments', element: <MedicalReportsSection /> },
-    { path: '/registerPatient', element: <RegisterPatient /> }
+    { path: '/registerPatient', element: <RegisterPatient /> },
+    { path: '/login', element: <Login /> }
 
     //Rutas del paciente
 
